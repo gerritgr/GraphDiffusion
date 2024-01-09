36c532a22f142c4d38f1e71c85d03a4588073c20
Gerrit G <gerritgr@gmail.com>
2024-01-09 15:16:59 +0100

    imports

---
+++ main.py
@@ -0,0 +1,42 @@
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+plt.rcParams['figure.dpi'] = 100  # Set this to 300 to get better image quality
+import seaborn as sns
+
+import networkx as nx
+import glob
+import random
+import os
+import traceback
+import time
+import copy
+import pickle
+import numpy as np
+import math
+from tqdm import tqdm
+import gzip
+
+from rdkit import Chem
+from rdkit.Chem import Draw
+
+import torch
+from torch import nn
+from torch.optim import Adam
+from torch.nn import Sequential as Seq
+from torch.nn import Linear as Lin
+import torch.nn.functional as F
+import torch_geometric
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import (
+    PNA,
+    GATv2Conv,
+    GraphNorm,
+    BatchNorm,
+    global_mean_pool,
+    global_add_pool
+)
+from torch_geometric.utils import erdos_renyi_graph, to_networkx, from_networkx, degree
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE
