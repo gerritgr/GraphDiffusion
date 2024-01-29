78c7c0f3c106f181fa3b67bdf4795c82e00328a4
Gerrit <gerritgr@users.noreply.github.com>
2024-01-29 10:53:03 +0100

    spiral works with stepbystep bridge

--- graphdiffusion/bridge.py
+++ graphdiffusion/bridge.py
@@ -25,7 +25,46 @@ class VectorBridgeColdDiffusion(nn.Module):
     
     def forward(self, pipeline, data_now, data_prediction, t_now, t_query, *args, **kwargs):
         x_0 = data_prediction
-        seed = int(random.random()*10000)
+        seed = torch.randint(0, 10000, (1,)).item()
         assert(t_now > t_query)
         x_tminus1 = data_now - pipeline.degradation(x_0, t_now, seed=seed) + pipeline.degradation(x_0, t_query, seed=seed)
+        return x_tminus1 
+
+
+
+class VectorBridgeStepByStep(nn.Module):
+    def __init__(self, node_feature_dim=1):
+        super(VectorBridgeStepByStep, self).__init__()
+        self.node_feature_dim = node_feature_dim
+    
+    def forward(self, pipeline, data_now, data_prediction, t_now, t_query, *args, **kwargs):
+        direction = data_prediction - data_now
+        direction = direction/torch.norm(direction)
+        seed = torch.randint(0, 10000, (1,)).item()
+        magnitude = torch.norm(pipeline.degradation(data_prediction, t_now, seed=seed) - pipeline.degradation(data_prediction, t_query, seed=seed))
+
+        x_tminus1 = data_now + direction * magnitude #(1.0-t_now)
+
+        if t_query > 1e-3:
+            x_tminus1 = x_tminus1 + torch.randn_like(x_tminus1) * magnitude/2.0
+
+        return x_tminus1 
+
+
+class VectorBridgeLinear(nn.Module):
+    def __init__(self, node_feature_dim=1):
+        super(VectorBridgeLinear, self).__init__()
+        self.node_feature_dim = node_feature_dim
+    
+    def forward(self, pipeline, data_now, data_prediction, t_now, t_query, *args, **kwargs):
+        direction = data_prediction - data_now
+        direction = direction/torch.norm(direction)
+        seed = torch.randint(0, 10000, (1,)).item()
+        #magnitude = torch.norm(pipeline.degradation(data_prediction, t_now, seed=seed) - pipeline.degradation(data_prediction, t_query, seed=seed))
+
+        x_tminus1 = data_now + direction * (1.0-t_now)
+
+        if t_query > 1e-3:
+            x_tminus1 = x_tminus1 + torch.randn_like(x_tminus1) * t_query/2.0
+
         return x_tminus1 
